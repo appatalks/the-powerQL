5c5c7e415f9f15c6abe935ac9293a515c39814cb
Steven Bennett <4163156+appatalks@users.noreply.github.com>
2023-11-03 23:45:24 -0500

    Update graphql_query.sh

--- graphql_query.sh
+++ graphql_query.sh
@@ -1,3 +1,3 @@
 # Example Usage:
-# ./codeql_query.sh graphpql_query.json
+# ./graphql_query.sh graphpql_query.json
 curl -s -H "Authorization: bearer $TOKEN" -X POST -d @$1 https://api.github.com/graphql | jq
